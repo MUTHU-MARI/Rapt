ff92f181742e9e9e778af9420278d8d22808e85a
Muthumari <110532427+MUTHU-MARI@users.noreply.github.com>
2022-12-06 10:25:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import AlignItemsList from './rough/cmts';
+import Rapt from './rough/receipe';
+//import Bg from './rough/bg';
+//import Rapt from './rough/receipe';
+function App() {
+  return (
+   
+  <Rapt/>
+  
+  );
+}
+export default App;
